2fad0a4499689c1604d400c069c2a304d47902af
=Anurag <=anuragsnd@gmail.com>
2025-03-24 23:12:18 +0530

    first simple project

---
+++ main.py
@@ -0,0 +1,19 @@
+# """This is just for an exercise"""
+# name=input("What is your name?\n")
+# print(f"How are you {name}")
+# print(len(name))
+
+
+# glass1 ="milk"
+# glass2 ="juice"
+# glass3 =glass1
+# glass1=glass2
+# glass2=glass3
+# print(glass2)
+# print(glass1)
+
+
+print("Welcome to Band name Generator")
+city_name = input("Which city you grew in?\n")
+pet_name= input("What's your pet name?\n")
+print(f"Your band name is {city_name }+ {pet_name}")
